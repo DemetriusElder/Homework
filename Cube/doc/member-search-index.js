1fde12734aad64eaf5dde8fbbf8d4854fd765866
Demetrius Elder <demetriuselderii@gmail.com>
2022-05-13 00:02:38 -0500

    Removed all the extra stuff

--- Cube/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"Cube","c":"Cube","l":"Cube(double)","u":"%3Cinit%3E(double)"},{"p":"Cube","c":"Cube","l":"surfaceArea()"},{"p":"Cube","c":"Cube","l":"volume()"}];updateSearchResults();
