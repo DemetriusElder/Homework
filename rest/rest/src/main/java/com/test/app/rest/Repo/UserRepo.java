541332837e72311dd28fa05e9d86f53354b5f459
Demetrius Elder <demetriuselderii@gmail.com>
2022-05-20 21:12:28 -0500

    Test

--- rest/rest/src/main/java/com/test/app/rest/Repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.app.rest.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.test.app.rest.Models.User;
-
-public interface UserRepo extends JpaRepository<User,Long>{
-	
-
-}
